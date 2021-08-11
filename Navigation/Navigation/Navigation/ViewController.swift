42d98762222dc8ad657f433079eca3a08f16e541
hililyy <joun406@gmail.com>
2021-08-11 11:38:07 +0900

    210811-1

--- Navigation/Navigation/Navigation/ViewController.swift
+++ Navigation/Navigation/Navigation/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController, EditDelegate {
     let imgOff = UIImage(named: "lamp_off.png")
     
     var isOn = true
+    var isZoom = false
+    var orgZoom = false
     
     @IBOutlet var txtMessage: UITextField!
     @IBOutlet var imgView: UIImageView!
@@ -31,6 +33,7 @@ class ViewController: UIViewController, EditDelegate {
         }
         editViewController.textMessage = txtMessage.text!
         editViewController.isOn = isOn
+        editViewController.isZoom = orgZoom
         editViewController.delegate = self
     }
     func didMessageEditDone(_ controller: EditViewController, message: String) {
@@ -44,6 +47,38 @@ class ViewController: UIViewController, EditDelegate {
             imgView.image = imgOff
             self.isOn = false
         }
+    }
+    
+    func didImageZoomDone(_ controller: EditViewController, isZoom: Bool) {
+        let scale:CGFloat = 2.0
+        var newWidth:CGFloat, newHeight:CGFloat
+        
+        if isZoom {
+            if orgZoom {
+                
+            } else {
+                self.isZoom = false
+                self.orgZoom = true
+                newWidth = imgView.frame.width*scale
+                newHeight = imgView.frame.height*scale
+                imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+            }
+            print("Zoom: true")
+        }
+        
+        else {
+            if orgZoom  {
+                self.isZoom = true
+                self.orgZoom = false
+                newWidth = imgView.frame.width/scale
+                newHeight = imgView.frame.height/scale
+                imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+            } else {
+                
+            }
+            print("Zoom: false")
+        }
+       
     }
 
 }
